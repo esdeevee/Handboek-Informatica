7c5499afc4eb31636c36b2359288059c255bac3e
esdeevee <76687782+esdeevee@users.noreply.github.com>
2025-12-29 16:03:37 +0100

    Add files via upload

---
+++ 3.1 Recursie/3.1:5/solution/modeloplossing 3.1:4.py
@@ -0,0 +1,11 @@
+def faculteit_iteratief(n):
+    product = 1
+    for i in range(1, n+1):
+        product = product * i
+
+
+def faculteit_recursief(n):
+    if n == 1:
+        return 1
+    else:
+        return n * faculteit_recursief(n-1)
