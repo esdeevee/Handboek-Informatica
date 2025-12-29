4061e437bd90c46af9f1ef0bfe435f4703e1f510
esdeevee <76687782+esdeevee@users.noreply.github.com>
2025-12-29 15:52:31 +0100

    Add files via upload

--- 3.1 Recursie/3.1:4/evaluation/genereer suite.yaml.py
+++ 3.1 Recursie/3.1:4/evaluation/genereer suite.yaml.py
@@ -5,15 +5,16 @@ def macht_direct(a, b):
 
 def macht_iteratief(a, b):
     product = 1
-    for i in range(a):
-        product = product * b
+    for i in range(b):
+        product = product * a
     return product
 
 def macht_recursief(a, b):
     if b == 1:
         return a
     else:
-        return a * product_recursief(a, b - 1)
+        return a * macht_recursief(a, b - 1)
+
 
         
 # suite.yaml voor de TESTed judge
--- 3.1 Recursie/3.1:4/solution/modeloplossing 3.1:4.py
+++ 3.1 Recursie/3.1:4/solution/modeloplossing 3.1:4.py
@@ -3,12 +3,12 @@ def macht_direct(a, b):
 
 def macht_iteratief(a, b):
     product = 1
-    for i in range(a):
-        product = product * b
+    for i in range(b):
+        product = product * a
     return product
 
 def macht_recursief(a, b):
     if b == 1:
         return a
     else:
-        return a * product_recursief(a, b - 1)
+        return a * macht_recursief(a, b - 1)
