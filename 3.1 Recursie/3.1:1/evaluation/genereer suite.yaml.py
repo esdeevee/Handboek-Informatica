941cef33b648d8eb16e7ce48d148933d82767bab
esdeevee <76687782+esdeevee@users.noreply.github.com>
2025-12-29 14:50:23 +0100

    Add files via upload

---
+++ 3.1 Recursie/3.1:1/evaluation/genereer suite.yaml.py
@@ -0,0 +1,29 @@
+from random import randint, gauss
+
+def aantal_manieren(n):
+    # n is een natuurlijk getal
+    if n <= 1:
+	    # voor een trap met 0 of 1 trede is het probleem al opgelost
+	    # er is maar 1 manier om een trap met 0 of 1 trede te beklimmen
+	    return 1
+    else:
+	    # het aantal trappen is minstens 2
+	    # we noteren het patroon dat we ingezien hadden
+	    return aantal_manieren(n - 1) + aantal_manieren(n - 2)
+
+        
+# suite.yaml voor de TESTed judge
+# wis alle gegevens in suite.yaml
+file = open("suite.yaml", "w")
+file.truncate()
+file.close()
+
+with open('suite.yaml', 'a') as file:
+    file.write('- tab: "aantal_manieren"\n')
+    file.write('  testcases:\n')
+    for i in range(5):
+        n = randint(5, 20)
+        file.write('    - expression: "aantal_manieren(' + str(n) + ')"\n')
+        file.write('      return: ' + str(aantal_manieren(n)) + '\n')
+
+    
